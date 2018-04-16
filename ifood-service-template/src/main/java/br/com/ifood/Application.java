c263e67aaf78ff1b4989c9c5fe3ec1e778ba2e33
Paulo Alexandre Machado <paulo.machado@sysmap.com.br>
2018-04-16 00:57:13 -0300

    Adjusting configurations

--- ifood-service-template/src/main/java/br/com/ifood/Application.java
+++ ifood-service-template/src/main/java/br/com/ifood/Application.java
@@ -3,10 +3,18 @@ package br.com.ifood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class Application {
+	
+	@RequestMapping("/")
+	  public String home() {
+	    return "Hello World";
+	  }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
