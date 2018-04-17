87fcfc86d665090bd166393fc3ff10b77a7ee679
Paulo Alexandre Machado <paulo.machado@sysmap.com.br>
2018-04-17 15:51:22 -0300

    Reorganize

--- ifood-service-template/src/main/java/br/com/ifood/Application.java
+++ ifood-service-template/src/main/java/br/com/ifood/Application.java
@@ -1,22 +1,47 @@
 package br.com.ifood;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @RestController
+@RequestMapping("/books")
 public class Application {
 	
-	@RequestMapping("/")
-	  public String home() {
-	    return "Hello World";
+	private List<Book> bookList = Arrays.asList(
+	        new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+	        new Book(2L, "Baeldung goes to the park", "Slavisa")
+	    );
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("")
+	public List<Book> findAllBooks() {
+		return bookList;
+	}
+
+	@GetMapping("/{bookId}")
+	public Book findBook(@PathVariable Long bookId) {
+		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+	}
+	
+	@Value("${lucky-word}") String luckyWord;
+	  
+	  @GetMapping("/lucky-word")
+	  public String showLuckyWord() {
+	    return "The lucky word is: " + luckyWord;
 	  }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 }
